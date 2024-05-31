14dba9bcc0d6a8195f9fad150f1760e54e5513b5
AnastasiyaFatPanda <kakuraanastasia@gmail.com>
2024-05-31 15:35:07 +0200

    task1

---
+++ Task1/ConsoleApp1/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter initial word: ");
+        string initialInput = Console.ReadLine() ?? "";
+
+        if (!CheckInput(initialInput)) return;
+
+        Console.Write("Enter another one to compare: ");
+        string anotherInput = Console.ReadLine() ?? "";
+
+        if (!CheckInput(anotherInput)) return;
+
+        List<KeyValuePair<char, int>> countedInitial = CountChars(initialInput);
+        List<KeyValuePair<char, int>> countedAnother = CountChars(anotherInput);
+
+        int attempts = 1;
+        while (IsMatch(countedInitial, countedAnother))
+        {
+            Console.Write("Enter word number {0} to compare: ", ++attempts);
+            anotherInput = Console.ReadLine() ?? "";
+            if (!CheckInput(anotherInput)) return;
+            countedAnother = CountChars(anotherInput);
+        }
+
+        StyledMessage("Incorrect input: letters do not match");
+        return;
+    }
+
+    static bool CheckInput(string input)
+    {
+        // empty
+        if (input.Length == 0)
+        {
+            StyledMessage("You cannot enter an empty word!");
+            return false;
+        }
+
+        // check length
+        if (input.Length < 8 || input.Length > 30)
+        {
+            StyledMessage("Your word must contain min 8 and max 30 letters!");
+            return false;
+        }
+
+        // only letters
+        if (!Regex.IsMatch(input, @"^[a-zA-Z]+$"))
+        {
+            StyledMessage("You can use only letters!");
+            return false;
+        }
+
+        return true;
+    }
+
+
+    static void StyledMessage(string message)
+    {
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+
+    static bool IsMatch(List<KeyValuePair<char, int>> countedInitial, List<KeyValuePair<char, int>> countedAnother)
+    {
+        return countedInitial.All(initialKeyValue => countedAnother.Contains(initialKeyValue));
+    }
+
+    static List<KeyValuePair<char, int>> CountChars(string input)
+    {
+        var chars = input
+            .Distinct()
+            .OrderBy(c => c)
+            .ToList();
+
+        var result = chars
+            .Select(c => new KeyValuePair<char, int>(c, input.Count(ch => ch == c)));
+
+        Console.WriteLine($"\n\n Check word: {input}");
+        foreach (KeyValuePair<char, int> person in result)
+        {
+            Console.WriteLine($"{person}");
+        }
+
+        return result.ToList();
+    }
+}
